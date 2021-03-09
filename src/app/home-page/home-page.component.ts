dad1404ca8372350ab693d8db0610168aaa34a7c
Viktor Syrytsia <syrytsiaviktor@gmail.com>
2021-03-09 22:41:51 +0200

    done home-page

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,3 +1,8 @@
+import { map, shareReplay } from 'rxjs/operators';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { ICategory } from './../models/category.model';
+import { Observable } from 'rxjs';
+import { CategoriesService } from './../services/categories.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +11,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home-page.component.scss']
 })
 export class HomePageComponent implements OnInit {
+  public isHandset$: Observable<boolean> = this.breakPointObserver
+  .observe([Breakpoints.Handset])
+  .pipe(
+    map(
+      (result) =>
+        result.breakpoints[
+          '(max-width: 599.98px) and (orientation: portrait)'
+        ]
+    ),
+    shareReplay()
+  );
+  categories$: Observable<ICategory[]>
 
-  constructor() { }
+  constructor(private categoriesService: CategoriesService, private breakPointObserver: BreakpointObserver) { }
 
   ngOnInit(): void {
+    this.categories$ = this.categoriesService.getAllCategories();
   }
 
 }
